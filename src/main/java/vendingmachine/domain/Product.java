3c23b67fad12dae0ead9447ad7e392cf9fc1c190
iamtaehoon <kimth9981@naver.com>
2021-12-13 22:00:06 +0900

    feat(): 입력한 상품의 이름과 가격 검증 기능 구현

--- src/main/java/vendingmachine/domain/Product.java
+++ src/main/java/vendingmachine/domain/Product.java
@@ -15,8 +15,40 @@ public class Product {
         // 1. 세 개로 나눈다.
         ArrayList<String> params = validateParamCnt(eachProductInfo);
         // 2. 이름을 검사한다.
+        String name = validateName(params.get(PRODUCT_NAME_IDX));
         // 3. 금액을 검사한다.
+        Price price = validatePrice(params.get(PRODUCT_PRICE_CNT));
         // 4. 개수를 검사한다.
+        // validateQuantity(params.get(PRODUCT_QUANTITY_CNT));
+
+    }
+
+    private Price validatePrice(String priceInput) {
+        priceInput = validateItIsEmpty(priceInput);
+        int price = StringUtils.convertStringToInt(priceInput);
+        if (price < 100) {
+            throw new IllegalArgumentException("상품의 가격이 100원 미만인 값이 있습니다.");
+        }
+        if (price < 0) {
+            throw new IllegalArgumentException("상품의 가격은 0 보다 커야 합니다.");
+        }
+        return new Price(price);
+    }
+
+    private String validateName(String name) {
+        name = validateItIsEmpty(name);
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("상품의 이름 중 다섯 글자를 초과하는 이름이 있습니다.");
+        }
+        return name;
+    }
+
+    private String validateItIsEmpty(String name) {
+        name = name.trim();
+        if (name.isEmpty()) {
+            throw new IllegalArgumentException("입력된 정보에 공백이 있습니다.");
+        }
+        return name;
     }
 
     private ArrayList<String> validateParamCnt(String eachProductInfo) {
--- src/test/java/vendingmachine/domain/ProductTest.java
+++ src/test/java/vendingmachine/domain/ProductTest.java
@@ -1,7 +1,10 @@
 package vendingmachine.domain;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
+import static vendingmachine.Constant.*;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -21,50 +24,57 @@ class ProductTest {
     @Test
     @DisplayName("상품 이름에 공백이 있을 경우 예외를 발생시킨다.")
     void 상품_이름_공백_예외() {
-        new Product(",1000,10");
+        assertThatThrownBy(() -> new Product(",1000,10")).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("입력된 정보에 공백이 있습니다.");
     }
 
     @Test
     @DisplayName("상품 이름이 다섯글자를 초과한 경우 예외를 발생시킨다.")
     void 상품_이름_다섯글자_초과_예외() {
-        new Product("미에로화이바,1000,10");
+        assertThatThrownBy(() -> new Product("미에로화이바,1000,10")).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("상품의 이름 중 다섯 글자를 초과하는 이름이 있습니다.");
     }
 
     // 가격 조건 틀린 경우
-
     @Test
     @DisplayName("상품 가격에 공백이 있을 경우 예외를 발생시킨다.")
     void 상품_가격_공백_예외() {
-        new Product("콜라,,10");
+        assertThatThrownBy(() -> new Product("콜라,,10")).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("입력된 정보에 공백이 있습니다.");
     }
 
     @Test
     @DisplayName("상품 가격이 숫자가 아닌 경우 예외를 발생시킨다.")
     void 상품_가격_숫자_아님_예외() {
-        new Product("콜라,a,10");
+        assertThatThrownBy(() ->  new Product("콜라,a,10")).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("숫자를 입력해 주세요.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"콜라,1,1", "콜라,101,0", "스프라이트,1024,10", "트코콜라,6089,100", "콜라,10003,13", "사,1008,84"})
     @DisplayName("상품의 가격이 동전 최소단위로 나누어떨어지지 않으면 예외를 발생시킨다.")
-    void 가격_10으로_나누어떨어지지_않음_예외() {
-
+    void 가격_10으로_나누어떨어지지_않음_예외(String input) {
+        assertThatThrownBy(() -> new Product(input)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining(String.format("금액은 %d로 나누어 떨어져야 합니다.", COIN_MIN_VALUE));
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"콜라,0,1", "콜라,-1,0", "스프라이트,-2,10", "트코콜라,-100,100", "콜라,-9487,13", "사,-1461,84"})
     @DisplayName("상품 가격이 0이거나, 음수면 예외를 발생시킨다.")
-    void 가격_0보다_작거나_같음_예외() {
-
+    void 가격_0보다_작거나_같음_예외(String input) {
+        assertThatThrownBy(() ->  new Product(input)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("상품의 가격은 0 보다 커야 합니다.");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"콜라,90,1", "콜라,80,0", "스프라이트,30,10", "트코콜라,10,100"})
     @DisplayName("상품 가격이 100원보다 작으면 예외를 발생시킨다.")
-    void 가격_100원_미만_오류() {
-
+    void 가격_100원_미만_오류(String input) {
+        assertThatThrownBy(() ->  new Product(input)).isInstanceOf(IllegalArgumentException.class)
+            .hasMessageContaining("상품의 가격이 100원 미만인 값이 있습니다.");
     }
 
+    //수량은 아직 최신화 안시켜줌.
     // 수량 조건 틀린 경우
     @Test
     @DisplayName("상품 수량에 공백이 있을 경우 예외를 발생시킨다.")
