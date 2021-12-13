bf3aafb24fe74b121b5f19b7599b3993ad061e7d
iamtaehoon <kimth9981@naver.com>
2021-12-13 20:15:51 +0900

    chore(CoinGenerator): 내부 로직 작게 분리

--- src/main/java/vendingmachine/utils/CoinGenerator.java
+++ src/main/java/vendingmachine/utils/CoinGenerator.java
@@ -8,27 +8,29 @@ import vendingmachine.domain.Coin;
 import vendingmachine.domain.Price;
 
 public class CoinGenerator {
-    static LinkedHashMap<Coin, Integer> temp = new LinkedHashMap<>();
+    static LinkedHashMap<Coin, Integer> coins = new LinkedHashMap<>();
 
     public static LinkedHashMap<Coin, Integer> makeCoins(Price price) {
         int totalMoney = price.getValue();
-        System.out.println(totalMoney);
-        initialize(temp);
+        initialize(coins);
 
+        makeCoinsUntilSameAsInput(totalMoney);
+        return coins;
+    }
+
+    private static void makeCoinsUntilSameAsInput(int totalMoney) {
         while (totalMoney > 0) {
             int selectedCoinValue = Randoms.pickNumberInList(Arrays.asList(500, 100, 50, 10));
-            System.out.println(selectedCoinValue);
             Coin selectedCoin = Coin.valueOf(selectedCoinValue);
             if (selectedCoinValue <= totalMoney) {
                 totalMoney -= selectedCoinValue;
-                temp.put(selectedCoin, temp.get(selectedCoin) + 1);
+                coins.put(selectedCoin, coins.get(selectedCoin) + 1);
             }
         }
-        return temp;
     }
 
     private static void initialize(LinkedHashMap<Coin, Integer> temp) {
         temp.clear();
-        Arrays.stream(Coin.values()).forEach(coin -> temp.put(coin, 0));
+        Arrays.stream(Coin.values()).forEach(coin -> coins.put(coin, 0));
     }
 }
