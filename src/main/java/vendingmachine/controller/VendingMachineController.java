4f55b84ba397827e0a2963613fe80400688c972b
iamtaehoon <kimth9981@naver.com>
2021-12-13 19:43:17 +0900

    feat(): 자판기에서 금액을 입력받는 기능 구현

--- src/main/java/vendingmachine/controller/VendingMachineController.java
+++ src/main/java/vendingmachine/controller/VendingMachineController.java
@@ -14,9 +14,17 @@ public class VendingMachineController {
 
     public void run() {
         // 자판기에서 금액을 입력받는다.
-        int inputAccount = StringUtils.convertStringToInt(InputView.putMoneyInVendingMachine());
-        Price price = new Price(inputAccount);
-        // 입력받은 금액은 정상인가?
-        // 정상이면 service에 넣어주자.
+        putMoneyInVendingMachine();
+    }
+
+    private void putMoneyInVendingMachine() {
+        try {
+            int inputAccount = StringUtils.convertStringToInt(InputView.putMoneyInVendingMachine());
+            Price price = new Price(inputAccount);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            putMoneyInVendingMachine();
+        }
+
     }
 }
