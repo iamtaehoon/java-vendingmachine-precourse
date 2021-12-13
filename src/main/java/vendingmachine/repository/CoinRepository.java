592b9ba8dead256529ef7f334e71c18c45727e75
iamtaehoon <kimth9981@naver.com>
2021-12-13 20:34:47 +0900

    feat(): 동전의 개수를 출력하는 기능 구현

--- src/main/java/vendingmachine/repository/CoinRepository.java
+++ src/main/java/vendingmachine/repository/CoinRepository.java
@@ -1,16 +1,25 @@
 package vendingmachine.repository;
 
+import java.util.Collections;
 import java.util.LinkedHashMap;
 
 import vendingmachine.domain.Coin;
+import vendingmachine.view.OutputView;
 
 public class CoinRepository {
     private LinkedHashMap<Coin, Integer> coinRepository = new LinkedHashMap<>();
 
     public void putCoins(LinkedHashMap<Coin, Integer> coins) {
         coins.keySet().stream().forEach(coin -> coinRepository.put(coin, coins.get(coin)));
-        // coinRepository.keySet()
-        //     .stream()
-        //     .forEach(coin -> System.out.println(coin.getAmount() + "원 동전 개수 : " + coins.get(coin)));
+        OutputView.showCoinsInVendingMachine(this);
+    }
+
+    @Override
+    public String toString() {
+        StringBuffer temp = new StringBuffer();
+        coinRepository.keySet()
+            .stream()
+            .forEach(coin -> temp.append(coin.getAmount() + "원 - " + coinRepository.get(coin) + "개\n"));
+        return temp.toString();
     }
 }
--- src/main/java/vendingmachine/view/OutputView.java
+++ src/main/java/vendingmachine/view/OutputView.java
@@ -1,4 +1,13 @@
 package vendingmachine.view;
 
+import java.util.Map;
+
+import vendingmachine.repository.CoinRepository;
+
 public class OutputView {
+
+    public static void showCoinsInVendingMachine(CoinRepository coinRepository) {
+        System.out.println("\n자판기가 보유한 동전");
+        System.out.println(coinRepository);
+    }
 }
