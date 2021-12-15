8d4fa2dcc33f307f5ba02045c6521db8b635ab32
iamtaehoon <kimth9981@naver.com>
2021-12-15 14:44:34 +0900

    refactor(): 동전 넣는 로직 재활용 가능하게 변경

--- src/main/java/vendingmachine/repository/CoinRepository.java
+++ src/main/java/vendingmachine/repository/CoinRepository.java
@@ -1,5 +1,6 @@
 package vendingmachine.repository;
 
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 
 import vendingmachine.domain.Coin;
@@ -9,8 +10,14 @@ import vendingmachine.view.OutputView;
 public class CoinRepository {
     private LinkedHashMap<Coin, Integer> coinRepository = new LinkedHashMap<>();
 
+    public CoinRepository() {
+        Arrays.stream(Coin.values()).forEach(coin -> coinRepository.put(coin, 0));
+    }
+
     public void putCoins(LinkedHashMap<Coin, Integer> coins) {
-        coinRepository.putAll(coins);
+        for (Coin coin : coins.keySet()) {
+            coinRepository.put(coin, coinRepository.get(coin) + coins.get(coin));
+        }
         OutputView.showCoinsInVendingMachine(this);
     }
 
