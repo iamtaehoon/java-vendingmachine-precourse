5bc3a5e73f96f9f64684e377782c4a0a4069fa0c
iamtaehoon <kimth9981@naver.com>
2021-12-16 16:13:16 +0900

    feat(): 앱 구조 생성

--- src/main/java/vendingmachine/repository/CoinRepository.java
+++ src/main/java/vendingmachine/repository/CoinRepository.java
@@ -1,4 +1,16 @@
 package vendingmachine.repository;
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+
+import vendingmachine.domain.Coin;
+import vendingmachine.domain.Money;
+
 public class CoinRepository {
+    private LinkedHashMap<Coin, Integer> coinRepository = new LinkedHashMap<>();
+
+    public CoinRepository() {
+        coinRepository.clear();
+        Arrays.stream(Coin.values()).forEach(coin -> coinRepository.put(coin, 0));
+    }
 }
--- src/main/java/vendingmachine/repository/ProductRepository.java
+++ src/main/java/vendingmachine/repository/ProductRepository.java
@@ -1,4 +1,10 @@
 package vendingmachine.repository;
 
+import java.util.HashMap;
+
+import vendingmachine.domain.Product;
+
 public class ProductRepository {
+    private HashMap<String, Product> productRepository = new HashMap<>();
+
 }
