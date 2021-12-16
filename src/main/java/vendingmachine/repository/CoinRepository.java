2fcf3d94d6bb9381c40a370dbbe8d53a3921f56c
iamtaehoon <kimth9981@naver.com>
2021-12-16 16:38:15 +0900

    feat(): 랜덤으로 만들어진 동전을 coinRepository에 저장하는 기능 구현

--- src/main/java/vendingmachine/repository/CoinRepository.java
+++ src/main/java/vendingmachine/repository/CoinRepository.java
@@ -14,6 +14,10 @@ public class CoinRepository {
         Arrays.stream(Coin.values()).forEach(coin -> coinRepository.put(coin, 0));
     }
 
+    public void putCoinInVendingMachine(LinkedHashMap<Coin, Integer> coins) {
+        Arrays.stream(Coin.values())
+            .forEach(coin -> coinRepository.put(coin, coinRepository.get(coin) + coins.get(coin)));
+    }
     //금액이 들어오면 -> 동전을 만들어서 넣어준다. / 잘못된 금액인 경우 service 단계에서 잘라준다.
 
 }
